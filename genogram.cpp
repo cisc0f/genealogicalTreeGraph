aa2b32caab490a7700008e20e0abd70d477281f7
cisc0f <francesco@coacci.it>
2021-05-30 00:08:36 +0200

    Implemented structs

--- genogram.cpp
+++ genogram.cpp
@@ -1,3 +1,19 @@
 #include "genogram.h"
 
 using namespace gen;
+
+struct gen::node{
+  // Payload
+  std::string name;
+  std::string gender;
+  std::string birth;
+  std::string death;
+  // Structure
+  edge* edges;
+  node* next;
+};
+
+struct gen::edge{
+  node* name;
+  edge* next;
+};
